7eea75d77edd36f573f1fe469c0fdcb7cdae5aca
Pau <mozga.paulina@gmail.com>
2024-01-21 23:05:56 +0100

    some ux

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function displayHaiku(response) {
   new Typewriter("#haiku", {
     strings: response.data.answer,
     autoStart: true,
-    delay: "natural",
+    delay: 50,
     cursor: "",
   });
 }
@@ -13,11 +13,15 @@ function generateHaiku(event) {
   let topic1 = document.querySelector("#topic1");
   let topic2 = document.querySelector("#topic2");
   let key = "630f42beb1a6c0d29a44a494adtbfo6a";
-  let prompt = `write a haiku about ${topic1.value} and ${topic2.value}`;
+  let prompt = `write one haiku about ${topic1.value} and ${topic2.value} together`;
   let context =
-    "You are a haiku generator. You write a haiku with every of haiku rules. Every first line of Haiku has 5 syllables, the second line has 7 syllables, and the third has 5 syllables - this is very important. Print every verse in next line with using <br />.";
+    "You are a haiku generator. You write a haiku with every of haiku rules. Every first line of Haiku has 5 syllables, the second line has 7 syllables, and the third has 5 syllables - this is very important. Print every verse in next line with using <br />. sign at the end in new line as '- SheCodes AI' with bold effect ";
 
   let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+  let haikuElement = document.querySelector("#haiku");
+  haikuElement.classList.remove("hidden");
+  haikuElement.innerHTML = `<div class="blink"> . . . . . .</div>`;
 
   axios.get(url).then(displayHaiku);
 }
