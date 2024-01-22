ceb2b44a9b2abd43788a9690388ec9c04e0cbdbc
Pau <mozga.paulina@gmail.com>
2024-01-22 18:39:26 +0100

    improving UI

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ function generateHaiku(event) {
   let key = "630f42beb1a6c0d29a44a494adtbfo6a";
   let prompt = `write one haiku about ${topic1.value} and ${topic2.value} together`;
   let context =
-    "You are a haiku generator. You write a haiku with every of haiku rules. Every first line of Haiku has 5 syllables, the second line has 7 syllables, and the third has 5 syllables - this is very important. Print every verse in next line with using <br />. sign at the end in new line as '- SheCodes AI' with bold effect ";
+    "You are a haiku generator. You write a haiku with every of haiku rules. Every first line of Haiku has 5 syllables, the second line has 7 syllables, and the third has 5 syllables - this is very important. Print every verse in next line with using <br />. Always sign at the end in new line using <br /> as '- SheCodes AI' inside <strong> element ";
 
   let url = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
 
